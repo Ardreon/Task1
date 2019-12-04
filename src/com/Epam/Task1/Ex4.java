90c47f1ee230a022834e0c709ab43adc0687ceb8
George Cherkashyn <kretroll@gmail.com>
2019-12-04 02:26:21 +0200

    Task1/Ex4 Напишите консольное приложение, которое выполняет поворот квадратной матрицы размерностью N на 90° против часовой стрелки.

--- src/com/Epam/Task1/Ex4.java
+++ src/com/Epam/Task1/Ex4.java
@@ -1,19 +1,15 @@
 package com.Epam.Task1;
 
+import java.util.Scanner;
+
 public class Ex4 {
 
     public static void main(String[] args) {
-        //Кол-во строк и столбцов в квадратной матрице
-        int size = 5;
 
-        //Попытка ввести число элементов с клавиатуры не увенчалась успехом
-        /*
-        try {
-            size = System.in.read();
-        } catch (IOException e) {
-            System.err.println("Error " + e);
-        }
-        */
+        //Кол-во строк и столбцов в квадратной матрице
+        System.out.print("Задайте размер массива: ");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
 
         int[][] array = new int[size][size];
         int[][] array2 = new int[size][size];
